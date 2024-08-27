8ac0e153869d382443368233b608f466c5dff79f
Mark Wood <mark@markjwood.com>
2024-08-26 17:53:31 -0700

    add responsiveness

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,12 +12,12 @@ export default function SearchBar() {
     <form className="my-6 flex gap-3 justify-center" onSubmit={onSubmit}>
       <label
         htmlFor="recipe-search"
-        className="text-2xl flex gap-3 items-center"
+        className="text-xl md:text-2xl flex gap-3 items-center"
       >
         Find Recipes
         <input
           id="recipe-search"
-          className="text-xl border rounded p-1"
+          className="text-lg md:text-xl border rounded p-1"
           type="text"
           value={searchText}
           onChange={(e) => setSearchText(e.target.value)}
