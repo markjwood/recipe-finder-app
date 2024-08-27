615d06dc132421ecd5853bd291dbf6168f31537c
Mark Wood <mark@markjwood.com>
2024-08-26 17:34:57 -0700

    SearchBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import SearchBar from "./components/SearchBar";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-center pt-1">
-      Hello World
-    </h1>
+    <>
+      <SearchBar />
+    </>
   );
 }
 
