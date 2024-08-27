3cca5775af65a722b8608ec817ef69ec3be175f6
Mark Wood <mark@markjwood.com>
2024-08-26 17:54:09 -0700

    create RecipeList component

---
+++ src/components/RecipeList.jsx
@@ -0,0 +1,12 @@
+export default function RecipeList() {
+  return (
+    <div className="mx-auto my-20 w-4/5 lg:w-1/2 xl:w-1/2 bg-white rounded-xl py-4 xl:py-6 px-4 xl:px-8">
+      <h2 className="text-3xl xl:text-4xl font-bold text-center">Recipes</h2>
+      <ul>
+        <li>Recipe 1</li>
+        <li>Recipe 2</li>
+        <li>Recipe 3</li>
+      </ul>
+    </div>
+  );
+}
