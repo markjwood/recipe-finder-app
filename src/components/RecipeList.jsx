eebd2519caf0eaea52ee0c2545a700aac0a7cf31
Mark Wood <mark@markjwood.com>
2024-08-28 17:36:55 -0700

    pass {list} as prop and map over list inside <ul>

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,11 +1,11 @@
-export default function RecipeList() {
+export default function RecipeList({ list }) {
   return (
     <section className="mx-auto my-20 w-4/5 lg:w-1/2 xl:w-1/2 bg-white rounded-xl py-4 xl:py-6 px-4 xl:px-8">
       <h2 className="text-3xl xl:text-4xl font-bold text-center">Recipes</h2>
       <ul>
-        <li>Recipe 1</li>
-        <li>Recipe 2</li>
-        <li>Recipe 3</li>
+        {list.map((r) => (
+          <li key={r.recipe.uri}>{r.recipe.label}</li>
+        ))}
       </ul>
     </section>
   );
