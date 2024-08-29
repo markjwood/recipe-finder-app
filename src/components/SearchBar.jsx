eb1bf8d78469f5c3d8a792eee4cdaffea92dcb24
Mark Wood <mark@markjwood.com>
2024-08-29 16:40:51 -0700

    replace form element with react-router-dom's Form; manage searchText state in component; add name to input (for query param)

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,15 @@
-import { searchRecipes } from "../api/search";
+import { Form } from "react-router-dom";
+import { useState } from "react";
 
-export default function SearchBar({ searchText, onChange, setRecipeList }) {
-  async function onSubmit(e) {
-    e.preventDefault();
-
-    const list = await searchRecipes(searchText);
-    setRecipeList(list);
-  }
+export default function SearchBar() {
+  const [searchText, setSearchText] = useState("");
 
   return (
-    <form className="my-6 flex gap-3 justify-center" onSubmit={onSubmit}>
+    <Form
+      action="/recipes"
+      method="get"
+      className="my-6 flex gap-3 justify-center"
+    >
       <label
         htmlFor="recipe-search"
         className="md:text-lg lg:text-xl flex gap-3 items-center"
@@ -17,13 +17,14 @@ export default function SearchBar({ searchText, onChange, setRecipeList }) {
         Find Recipes
         <input
           id="recipe-search"
+          name="q" // sets name for query param
           className="input input-sm md:input-md input-bordered max-w-xs p-1"
           type="text"
           value={searchText}
-          onChange={(e) => onChange(e.target.value)}
+          onChange={(e) => setSearchText(e.target.value)}
         />
       </label>
       <button className="btn btn-sm md:btn-md btn-primary">Search</button>
-    </form>
+    </Form>
   );
 }
