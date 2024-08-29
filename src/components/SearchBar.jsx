969ec7b8471de48622ad01265f908febfcf206df
Mark Wood <mark@markjwood.com>
2024-08-29 10:03:24 -0700

    use daisyui classes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,20 +12,18 @@ export default function SearchBar({ searchText, onChange, setRecipeList }) {
     <form className="my-6 flex gap-3 justify-center" onSubmit={onSubmit}>
       <label
         htmlFor="recipe-search"
-        className="text-xl md:text-2xl flex gap-3 items-center"
+        className="text-lg md:text-xl flex gap-3 items-center"
       >
         Find Recipes
         <input
           id="recipe-search"
-          className="text-lg md:text-xl border rounded p-1"
+          className="input input-bordered max-w-xs"
           type="text"
           value={searchText}
           onChange={(e) => onChange(e.target.value)}
         />
       </label>
-      <button className="bg-sky-600 text-white rounded-lg font-bold px-4 hover:bg-sky-300 hover:text-sky-800 transition">
-        Search
-      </button>
+      <button className="btn btn-primary">Search</button>
     </form>
   );
 }
