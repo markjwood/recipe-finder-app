4f592f670210dea5b64c1481e9b08df468068dd7
Mark Wood <mark@markjwood.com>
2024-08-29 14:16:09 -0700

    add loader & route

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,12 +1,34 @@
-export default function RecipeList({ list }) {
+import { useLoaderData, useNavigation } from "react-router-dom";
+import { searchRecipes } from "../api/search";
+
+function RecipeList() {
+  const list = useLoaderData();
+
+  // get 'loading' state
+  const { state } = useNavigation();
+  const isLoading = state === "loading";
+
   return (
-    <section className="prose mx-auto my-20 w-4/5 lg:w-1/2 xl:w-1/2 bg-white rounded-xl py-4 xl:py-6 px-4 xl:px-8">
+    <section className="prose mx-auto my-20 w-4/5 max-w-2xl bg-white rounded-xl py-4 xl:py-6 px-4 xl:px-8">
       <h2 className="text-center">Recipes</h2>
-      <ul>
-        {list.map((r) => (
-          <li key={r.recipe.uri}>{r.recipe.label}</li>
-        ))}
-      </ul>
+      {isLoading ? (
+        <h3>getting recipes...</h3>
+      ) : (
+        <ul>
+          {list.map((r) => (
+            <li key={r.recipe.uri}>{r.recipe.label}</li>
+          ))}
+        </ul>
+      )}
     </section>
   );
 }
+
+async function loader({ params: { query } }) {
+  return await searchRecipes(query);
+}
+
+export const recipesListRoute = {
+  loader,
+  element: <RecipeList />,
+};
