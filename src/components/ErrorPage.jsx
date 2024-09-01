357b7a9f03c3c54433299c36567e7af3e7c2cb6e
Mark Wood <mark@markjwood.com>
2024-09-01 16:38:47 -0700

    create ErrorPage.jsx

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+import { useNavigate, useRouteError } from "react-router-dom";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  const navigate = useNavigate();
+
+  // check if in development
+  const isDev = import.meta.env.DEV;
+
+  useEffect(() => {
+    // redirect to home page after 5 seconds if not in development
+    if (!isDev) {
+      const timeout = setTimeout(() => {
+        navigate("/");
+      }, 5000);
+
+      return () => clearTimeout(timeout);
+    }
+  }, []);
+
+  return (
+    <div className="container mx-auto prose">
+      <h2>Something went wrong &ndash; {error.name}</h2>
+      <p>{error.message}</p>
+      {!isDev && (
+        <p>
+          Returning <Link to="/">home</Link> in 5 seconds.
+        </p>
+      )}
+
+      {/* show stack trace if in development */}
+      {isDev && <pre>{error.stack}</pre>}
+    </div>
+  );
+}
