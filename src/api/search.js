140992ec76fb3dc70d2b07d8d08683d6b56cd8fd
Mark Wood <mark@markjwood.com>
2024-09-12 11:26:23 -0700

    Netlify

--- src/api/search.js
+++ src/api/search.js
@@ -1,22 +1,23 @@
-import { base } from './base'
+import axios from 'axios'
 
 export function searchRecipes(query) {
 	// replace spaces in query with '+'
 	query = query.trim().replace(/\s+/g, '+')
 
 	const controller = new AbortController()
-	const app_key = import.meta.env.VITE_APP_KEY
-	const app_id = import.meta.env.VITE_APP_ID
-	const queryString = `v2?q=${query}&type=any&app_id=${app_id}&app_key=${app_key}`
+	const queryString = `?q=${query}`
 
-	const recipes = base
-		.get(queryString, { signal: controller.signal })
+	const recipes = axios
+		.get(`/.netlify/functions/recipeSearch${queryString}`, {
+			signal: controller.signal,
+		})
 		.then(res => {
 			// return null if no hits
-			return res.data?.hits ?? null
+			return res.data ?? null
 		})
 		.catch(err => {
 			if (err.name === 'AbortError') return
+			console.error('Error fetching recipes:', err)
 		})
 
 	return recipes
